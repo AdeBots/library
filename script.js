f1d35749cc8f097ada61c41b322b61f32661c482
AdeBots <190976863+AdeBots@users.noreply.github.com>
2024-12-26 00:42:14 +0100

    Loop is working well

--- script.js
+++ script.js
@@ -7,19 +7,20 @@ function Book(title, author, noOfPages, readStatus) {
     this.readStatus = readStatus;
 }
 
-function addBookToLibrary() {
-    const title;
-    const author;
-    const noOfPages;
-    const readStatus;
-    const newBook = new Book(title, author, noOfPages, readStatus);
-    myLibrary.push(newBook);
-}
+// function addBookToLibrary() {
+//     const title;
+//     const author;
+//     const noOfPages;
+//     const readStatus;
+//     const newBook = new Book(title, author, noOfPages, readStatus);
+//     myLibrary.push(newBook);
+// }
 
 
-const book1 = new Book("Iron Man", "Steve Rogers", 185, read);
-const book2 = new Book("Captain America", "Tony Stark", 124, not read);
-myLibrary.push(book1, book2);
+const book1 = new Book("Iron Man", "Steve Rogers", 185, "read");
+const book2 = new Book("Captain America", "Tony Stark", 124, "not read");
+myLibrary.push(book1);
+myLibrary.push(book2);
 for (let i = 0; i < myLibrary.length; i++) {
     console.log(myLibrary[i].title);
     console.log(myLibrary[i].author);
