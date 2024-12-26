552cbd96d59a3715601a7e010b588898d6ba066f
AdeBots <190976863+AdeBots@users.noreply.github.com>
2024-12-26 11:38:30 +0100

    Corrected some errors

--- script.js
+++ script.js
@@ -7,30 +7,56 @@ function Book(title, author, noOfPages, readStatus) {
     this.readStatus = readStatus;
 }
 
-function createBook(bookData){
+
+
+function createBook(bookData, index){
     const book = document.createElement("div");
     const bookTitle = document.createElement("p");
     const bookAuthor = document.createElement("p");
     const bookNoOfPages = document.createElement("p");
     const bookReadStatus = document.createElement("p");
+    const removeBtn = document.createElement("button");
 
     book.classList.add("book");
     bookTitle.classList.add("title");
     bookAuthor.classList.add("author");
     bookNoOfPages.classList.add("noOfPages");
     bookReadStatus.classList.add("readStatus");
+    removeBtn.classList.add("removeBtn");
 
     bookTitle.innerText = bookData.title;
     bookAuthor.innerText = bookData.author;
     bookNoOfPages.innerText = bookData.noOfPages;
     bookReadStatus.innerText = bookData.readStatus;
+    removeBtn.innerText = "Remove Book"
 
     book.appendChild(bookTitle);
     book.appendChild(bookAuthor);
     book.appendChild(bookNoOfPages);
     book.appendChild(bookReadStatus);
+    book.appendChild(removeBtn);
 
+    book.setAttribute('data-index', index);
+
+    removeBtn.addEventListener("click", () => {
+        removeBookFromLibrary(index);
+    })
+    
     return book;    
+}
+
+function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1);
+    const bookElement = document.querySelector(`.book[data-index='${index}']`);
+    library.removeChild(bookElement);
+    updateBookIndices();
+}
+
+function updateBookIndices() {
+    const bookElements = document.querySelectorAll('.book');
+    bookElements.forEach((bookElement, newIndex) => {
+        bookElement.setAttribute('data-index', newIndex);
+    });
 }
 
 function clearInputs() {
@@ -49,12 +75,13 @@ function addBookToLibrary() {
     const newBook = new Book(title, author, noOfPages, readStatus);
     myLibrary.push(newBook);
 
-    const bookElement = createBook(newBook);
+    const bookElement = createBook(newBook, myLibrary.length - 1);
     library.appendChild(bookElement);
-
+    
     clearInputs();
 
 }
+
 
 const addBookBtn = document.querySelector(".addNewBook");
 const dialog = document.querySelector("dialog");
